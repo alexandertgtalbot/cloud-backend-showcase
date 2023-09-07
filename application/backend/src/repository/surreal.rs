e80ec28d47a02be61df47db68710e14b37da3936
Alex Talbot <alexandertgtalbot@gmail.com>
2023-09-07 11:34:59 +0100

    Add Initial Repository Module
    - Implement an initial abstraction layer to support multiple
      repositories/databases/engines.
    - Add initial implementation for SurrealDB.

---
+++ application/backend/src/repository/surreal.rs
@@ -0,0 +1,57 @@
+// Path: github.com/alexandertgtalbot/cloud-backend-showcase/application/backend/src/repository/surreal.rs
+//
+// Summary: SurrealDB repository receiver functions.
+//
+// Operations:
+// - User:
+//   - Create user.
+//   - Get user by username.
+//   - Update user by username.
+//   - Delete user by username.
+
+use crate::models::user::User;
+use crate::repository::{Repository, RepositoryError};
+
+pub struct SurrealdbRepository {
+    config: std::sync::Arc<crate::configs::Config>,
+    db: None,
+}
+
+impl SurrealdbRepository {
+    pub async fn init(config: std::sync::Arc<crate::configs::Config>) -> Self {
+        todo!()
+    }
+}
+
+#[async_trait::async_trait]
+impl Repository for SurrealdbRepository {
+    async fn create_user(
+        &self,
+        username: String,
+        user_data: User,
+    ) -> Result<User, RepositoryError> {
+        todo!()
+    }
+
+    async fn get_user(
+        &self,
+        username: String,
+    ) -> Result<User, RepositoryError> {
+        todo!()
+    }
+
+    async fn update_user(
+        &self,
+        username: String,
+        user_data: User,
+    ) -> Result<User, RepositoryError> {
+        todo!()
+    }
+
+    async fn delete_user(
+        &self,
+        username: String,
+    ) -> Result<User, RepositoryError> {
+        todo!()
+    }
+}
