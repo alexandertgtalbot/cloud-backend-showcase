3d31f834cfd4727ba788d94e2325cc92e495ac33
Alex Talbot <alexandertgtalbot@gmail.com>
2023-09-08 19:53:07 +0100

    Add User API Endpoint
    - Add create user that creates or updates a user.

--- application/backend/src/api/user.rs
+++ application/backend/src/api/user.rs
@@ -10,19 +10,47 @@
 
 use crate::models::user::User;
 use crate::repository::Engine;
+use crate::repository::RepositoryError::{Duplicate, NotFound, Unavailable};
+
 use actix_web::{
     delete, get, put,
     web::{Data, Json, Path},
     HttpResponse, HttpResponseBuilder,
 };
+use validator::Validate;
 
 #[put("/{username}")]
 async fn create_user(
-    _db: Data<Engine>,
-    _path: Path<String>,
-    _payload: Json<User>,
+    db: Data<Engine>,
+    path: Path<String>,
+    payload: Json<User>,
 ) -> HttpResponseBuilder {
-    todo!()
+    let username = path.into_inner();
+    let mut user = payload.into_inner();
+
+    if user.username.is_none() {
+        user.username = Some(username.clone());
+    }
+
+    if user.validate().is_err() {
+        return HttpResponse::BadRequest();
+    }
+
+    match db.create_user(username.clone(), user.clone()).await {
+        Ok(_) => HttpResponse::NoContent(),
+        Err(error) => match error {
+            Duplicate(_) => match db.update_user(username, user).await {
+                Ok(_) => HttpResponse::NoContent(),
+                Err(error) => match error {
+                    Duplicate(_) => HttpResponse::Conflict(),
+                    NotFound(_) => HttpResponse::NotFound(),
+                    Unavailable(_) => HttpResponse::ServiceUnavailable(),
+                },
+            },
+            NotFound(_) => HttpResponse::NotFound(),
+            Unavailable(_) => HttpResponse::ServiceUnavailable(),
+        },
+    }
 }
 
 #[get("/{username}")]
