48cb49a552a64201cd75d84a251279d4dd5ac397
Alex Talbot <5956898+alexandertgtalbot@users.noreply.github.com>
2023-09-08 13:56:15 +0100

    Add User Model Unit Tests (#10)
    * Add User Model Unit Tests
    
    * Add User Model Validation
    
    - Add validation to satisfy all unit tests.
    - Update README so that the backend chacks badge references `main`.
    - Update README adding MIT license badge linking to license.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 [![codecov](https://codecov.io/gh/alexandertgtalbot/cloud-backend-showcase/graph/badge.svg?token=M2GJ1VPHTT)](https://codecov.io/gh/alexandertgtalbot/cloud-backend-showcase)
-[![Backend Checks](https://github.com/alexandertgtalbot/cloud-backend-showcase/actions/workflows/backend-checks.yml/badge.svg?branch=feature%2Fadd-initial-workflows)](https://github.com/alexandertgtalbot/cloud-backend-showcase/actions/workflows/backend-checks.yml)
+[![Backend Checks](https://github.com/alexandertgtalbot/cloud-backend-showcase/actions/workflows/backend-checks.yml/badge.svg?branch=main)](https://github.com/alexandertgtalbot/cloud-backend-showcase/actions/workflows/backend-checks.yml)
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/alexandertgtalbot/cloud-backend-showcase/blob/main/LICENSE)
 
 # Cloud Backend Showcase
 A Rust-Based RESTful API, cloud infrastructure and CI/CD Showcase.
--- application/backend/src/models/user.rs
+++ application/backend/src/models/user.rs
@@ -2,11 +2,119 @@
 //
 // Summary: User model.
 
+use lazy_static::lazy_static;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
+use validator::{Validate, ValidationError};
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+lazy_static! {
+    static ref ISO_8601_DATE: Regex = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, Validate)]
 #[serde(rename_all = "camelCase")]
 pub struct User {
+    #[validate(regex = "ISO_8601_DATE")]
+    #[validate(custom = "validate_date_of_birth")]
     pub date_of_birth: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[validate(custom = "validate_username")]
     pub username: Option<String>,
+}
+
+fn validate_date_of_birth(date_of_birth: &str) -> Result<(), ValidationError> {
+    let parsed_date = chrono::naive::NaiveDate::parse_from_str(date_of_birth, "%F");
+    if parsed_date.is_ok() && parsed_date.unwrap() < chrono::Utc::now().date_naive() {
+        return Ok(());
+    }
+    Err(ValidationError::new(
+        "Date of birth should have the format YYYY-MM-DD and be in the past",
+    ))
+}
+
+fn validate_username(username: &str) -> Result<(), ValidationError> {
+    if username.chars().all(char::is_alphabetic) && !username.is_empty() {
+        return Ok(());
+    }
+    Err(ValidationError::new(
+        "The username should only contain alphabetic characters",
+    ))
+}
+
+#[cfg(test)]
+mod validation {
+    use super::*;
+
+    #[test]
+    fn accept_iso_8601_compliant_date_in_the_past() {
+        let user = User {
+            date_of_birth: String::from("2023-09-05"),
+            username: None,
+        };
+        assert!(user.validate().is_ok());
+    }
+
+    #[test]
+    fn reject_iso_8601_compliant_date_for_today() {
+        let user = User {
+            date_of_birth: chrono::Utc::now().date_naive().to_string(),
+            username: None,
+        };
+        assert!(user.validate().is_err());
+    }
+
+    #[test]
+    fn reject_single_digit_month_and_day() {
+        let user = User {
+            date_of_birth: String::from("2023-9-6"),
+            username: None,
+        };
+        assert!(user.validate().is_err());
+    }
+
+    #[test]
+    fn reject_freedom_format() {
+        let user = User {
+            date_of_birth: String::from("09-06-2023"),
+            username: None,
+        };
+        assert!(user.validate().is_err());
+    }
+
+    #[test]
+    fn accept_valid_username() {
+        let user = User {
+            date_of_birth: String::from("2023-09-05"),
+            username: Some(String::from("test")),
+        };
+        assert!(user.validate().is_ok());
+    }
+
+    #[test]
+    fn reject_username_with_digits() {
+        let user = User {
+            date_of_birth: String::from("2023-09-05"),
+            username: Some(String::from("test123")),
+        };
+        assert!(user.validate().is_err());
+    }
+
+    #[test]
+    fn reject_username_with_special_characters() {
+        let user = User {
+            date_of_birth: String::from("2023-09-05"),
+            username: Some(String::from("test!")),
+        };
+        assert!(user.validate().is_err());
+    }
+
+    #[test]
+    fn reject_username_of_zero_legth() {
+        let user = User {
+            date_of_birth: String::from("2023-09-05"),
+            username: Some(String::from("")),
+        };
+        assert!(user.validate().is_err());
+    }
 }
